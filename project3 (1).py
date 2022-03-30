0ad7b523c851fa139dc4902789cd77fce2018207
leocorelli <leocorelli>
2022-03-30 16:03:59 +0000

    add notebook

---
+++ project3 (1).py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+import pyspark.pandas as ps
+
+# COMMAND ----------
+
+df = ps.read_csv("dbfs:/FileStore/shared_uploads/lmc121@duke.edu/winequality_red-2.csv")
+df.head(30)
+
+# COMMAND ----------
+
+df.describe()
+
+# COMMAND ----------
+
+import databricks.automl
+
+# COMMAND ----------
+
+databricks.automl.regress(df, target_col= "quality")
